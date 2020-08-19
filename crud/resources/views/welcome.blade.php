81084e0c2003e5b7e82a8e63f9e3561ebdc11671
Iresh Maduranga Ranaweera <m.iresh.main@gmail.com>
2020-08-19 13:57:10 +0530

    Change the welcome.php

--- crud/resources/views/welcome.blade.php
+++ crud/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pramix
                 </div>
 
                 <div class="links">
